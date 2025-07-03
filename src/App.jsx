15b1e14a131faef374f130dd5c12f830c3ff1a72
nitin <nitinsah20@gmail.com>
2025-07-03 12:18:02 +0530

    Axios api get mathode applly

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import New from "./pages/New";
 import MainLayout from "./layout/MainLayout";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-// import Product from "./pages/Product";
+import Product from "./pages/Product";
 
 const App = () => {
   const [cartCount, setCartCount] = useState(0);
@@ -29,7 +29,7 @@ const App = () => {
             <Route path="/new" element={<New />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
-            {/* <Route path="/product" element={<Product/>}></Route> */}
+            <Route path="/product" element={<Product/>}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
